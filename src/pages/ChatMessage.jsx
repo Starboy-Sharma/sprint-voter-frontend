e3f335ea4103583936be5eec2f2e799ae79f7c1d
Pankaj Sharma <pankaj.sharma@emids.com>
2023-01-24 01:23:44 +0530

    add sprint title and description

--- src/pages/ChatMessage.jsx
+++ src/pages/ChatMessage.jsx
@@ -1,14 +1,128 @@
-import React from 'react';
+import { useState } from 'react';
+import {
+  Modal,
+  Button,
+  Form,
+  FloatingLabel,
+  Toast,
+  ToastContainer,
+} from 'react-bootstrap';
 
-export default function ChatMessage({ title, description }) {
+import { FcReading } from 'react-icons/fc';
+
+// SPRINT MODAL FORM
+const ModalForm = function ({ setShowModal, showModal, setSprintData }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [show, setShow] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSprintData({ title, description });
+    setShow(true);
+    setShowModal(false);
+  };
+
   return (
-    <div className="chat-message">
-      <div className="message">
-        <h3> {title} </h3>
-
-        <p> {description} </p>
+    <>
+      <div
+        aria-live="polite"
+        aria-atomic="true"
+        className="bg-dark position-relative"
+        style={{ minHeight: '240px' }}
+      >
+        <ToastContainer position="top-end">
+          <Toast
+            onClose={() => setShow(false)}
+            show={show}
+            delay={3000}
+            autohide
+            position="top-end"
+          >
+            <Toast.Header className="text-dark">
+              <FcReading fontSize={26} className="mr-2" />
+              <strong className="me-auto"> Success 🎉</strong>
+            </Toast.Header>
+            <Toast.Body className="text-dark">
+              Woohoo, you have updated the title and description
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
       </div>
-      <button className="btn btn-warning"> Add </button>
-    </div>
+
+      <Modal show={showModal} onHide={setShowModal}>
+        <Modal.Header>
+          <Modal.Title className="text-dark">Add your task details</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body className="text-dark">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="title" className="mb-3">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Title"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                required="Title is required"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="description">
+              <FloatingLabel controlId="floatingTextarea2" label="Description">
+                <Form.Control
+                  as="textarea"
+                  value={description}
+                  onChange={(event) => setDescription(event.target.value)}
+                  placeholder="Leave a comment here"
+                  style={{ height: '100px', resize: 'none' }}
+                  required="Description is required"
+                />
+              </FloatingLabel>
+            </Form.Group>
+
+            <Button
+              variant="dark"
+              type="submit"
+              className="mt-3"
+              style={{ float: 'right' }}
+            >
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default function ChatMessage() {
+  const [showModal, setShowModal] = useState(false);
+  const [sprintData, setSprintData] = useState({
+    title: 'Your title will appear here',
+    description: 'You can add your own title & description here 👉',
+  });
+
+  return (
+    <>
+      {showModal ? (
+        <ModalForm
+          setShowModal={setShowModal}
+          showModal={showModal}
+          setSprintData={setSprintData}
+        />
+      ) : null}
+
+      <div className="chat-message">
+        <div className="message">
+          <h3> {sprintData.title} </h3>
+
+          <p> {sprintData.description} </p>
+        </div>
+        <button className="btn btn-warning" onClick={() => setShowModal(true)}>
+          Add
+        </button>
+      </div>
+    </>
   );
 }
